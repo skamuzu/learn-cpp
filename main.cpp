64fdd672dc69b198fd5e695a4fa487f9a7da7d8c
Selom Amuzu <selomka1@gmail.com>
2025-11-02 01:27:49 +0000

    Intro to functions

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
-int main(){
-    std::cout << "Enter a number: " ;
-    int x{};
-    std::cin >> x;
-    std::cout << '\n' << "Double that number is: " << x*2;
-    return 0;
+int getValueFromUserUB() // this function returns an integer value
+{
+ 	std::cout << "Enter an integer: ";
+	int input{};
+	std::cin >> input;
+
+	// note: no return statement
+}
+
+int main()
+{
+	int num { getValueFromUserUB() }; // initialize num with the return value of getValueFromUserUB()
+
+	std::cout << num << " doubled is: " << num * 2 << '\n';
+
+	return 0;
 }
